0a122e773cc5e84c6a07c709645c9c25d8735541
hariharan-yeswanth <125393254+hariharan-yeswanth@users.noreply.github.com>
2023-02-27 13:57:52 +0530

    Uploaded assignment-11

---
+++ Assignment11/index11.js
@@ -0,0 +1,13 @@
+Calculator={
+    add:(num1,num2)=> num1+num2,
+    sub:(num1,num2)=> num1-num2,
+    mul:(num1,num2)=> num1*num2,
+    div:(num1,num2)=> num1/num2
+
+}
+num1=20,num2=10;
+console.log("The addition of two number is : "+Calculator.add(num1,num2));
+console.log("The Subtraction of two number is : "+Calculator.sub(num1,num2));
+console.log("The Multiplication of two number is : "+Calculator.mul(num1,num2));
+console.log("The Division of two number is : "+Calculator.div(num1,num2));
+
